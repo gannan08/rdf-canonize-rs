1664c64ddb1b9188b6b42755002db27f495d56c9
Ganesh Annan <gannan@digitalbazaar.com>
2020-12-26 03:01:55 -0500

    Cleanup and add performance optimizations.

--- src/permuter.rs
+++ src/permuter.rs
@@ -1,15 +1,19 @@
-use std::collections::HashMap;
-
-type DirectionMap = HashMap<String, bool>;
-
-#[derive(Clone, Debug, PartialEq)]
-pub struct Permuter {
-  current: Vec<String>,
-  done: bool,
-  dir: DirectionMap,
+#[derive(Copy, Clone, Debug, PartialEq)]
+struct PermutationElement<'a> {
+  direction: bool,
+  value: &'a str,
+  empty: bool,
 }
 
-impl Permuter {
+type PermutationElements<'a> = Vec<PermutationElement<'a>>;
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct Permuter<'a> {
+  current: PermutationElements<'a>,
+  done: bool,
+}
+
+impl Permuter<'_> {
   /**
    * A Permuter iterates over all possible permutations of the given array
    * of elements.
@@ -17,98 +21,78 @@ impl Permuter {
    * @param list the array of elements to iterate over.
    */
   pub fn new(list: &mut Vec<String>) -> Permuter {
-    let current = &mut list[..];
+    let list_clone = &mut list[..];
     // original array
-    current.sort();
+    list_clone.sort();
     // indicates whether there are more permutations
     let done = false;
-    // directional info for permutation algorithm
-    let mut dir = DirectionMap::new();
-    for v in current.iter() {
-      dir.insert(v.to_string(), true);
+    let mut current = vec![];
+    for value in list_clone.iter() {
+      current.push(PermutationElement {
+        direction: true,
+        value,
+        empty: false,
+      })
     }
-    let mut vec = vec![];
-    vec.extend_from_slice(current);
-    Permuter {
-      current: vec,
-      done,
-      dir,
+    Permuter { current, done }
+  }
+}
+
+impl Iterator for Permuter<'_> {
+  type Item = Vec<String>;
+  fn next(&mut self) -> Option<Self::Item> {
+    if self.done {
+      return None;
     }
-  }
-
-  /**
-   * Returns true if there is another permutation.
-   *
-   * @return true if there is another permutation, false if not.
-   */
-  pub fn has_next(&self) -> bool {
-    !self.done
-  }
-
-  /**
-   * Gets the next permutation. Call has_next() to ensure there is another one
-   * first.
-   *
-   * @return the next permutation.
-   */
-  pub fn next(&mut self) -> Vec<String> {
     // copy current permutation to return it
     let current = &mut self.current;
-    let dir = &mut self.dir;
-    let rval = current.to_vec();
+    let rval = current.iter().map(|x| x.value.to_string()).collect();
     /* Calculate the next permutation using the Steinhaus-Johnson-Trotter
     permutation algorithm. */
 
     // get largest mobile element k
     // (mobile: element is greater than the one it is looking at)
-    let mut k: Option<String> = None;
-    let mut k_val = String::from("");
-    let mut k_is_none = true;
+    let mut k = PermutationElement {
+      direction: true,
+      value: "",
+      empty: true,
+    };
     let mut pos = 0;
     let length = current.len();
-    for (i, element) in current.iter().enumerate() {
-      let left = dir.get(element).unwrap();
-      if let Some(tmp_k) = &k {
-        k_is_none = false;
-        k_val = tmp_k.to_string();
-      } else {
-        k_is_none = true;
-        k_val = "".to_string();
-      }
-      if (k_is_none || element > &k_val)
-        && ((*left && i > 0 && element > &current[i - 1])
-          || (!left && i < (length - 1) && element > &current[i + 1]))
+    for (i, permutator_elment) in current.iter().enumerate() {
+      let left = permutator_elment.direction;
+      let element = permutator_elment.value;
+      if (k.empty || element > k.value)
+        && ((left && i > 0 && element > current[i - 1].value)
+          || (!left && i < (length - 1) && element > current[i + 1].value))
       {
-        k = Some(element.to_string());
+        k = current[i];
         pos = i;
       }
     }
 
     // no more permutations
-    if k_is_none {
+    if k.empty {
       self.done = true;
     } else {
       // swap k and the element it is looking at
-      let swap = if *dir.get(&k_val).unwrap() {
+      let swap = if current[pos].direction {
         pos - 1
       } else {
         pos + 1
       };
-      let swap_val = &current[pos].clone();
-      current[pos] = current[swap].to_string();
-      current[swap] = swap_val.to_string();
+      current[pos] = current[swap];
+      current[swap] = k;
 
       // reverse the direction of all elements larger than k
-      for element in current.iter() {
-        if element > &mut k_val.to_string() {
-          dir.insert(
-            element.to_string(),
-            !dir.get(&element.to_string()).unwrap(),
-          );
+      for permutator_elment in current.iter_mut() {
+        let element = &permutator_elment.value;
+        if element > &k.value {
+          permutator_elment.direction = !permutator_elment.direction;
         }
       }
     }
 
-    rval
+    Some(rval)
   }
 }
